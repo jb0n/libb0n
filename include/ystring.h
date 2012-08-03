74b0ea35f7812170b3127baa22d9a8d23afe4841
jb0n <bubble.mack@gmail.com>
2012-08-03 16:18:22 -0400

    added FreeBSD license to all files. Also added lstrip, rstrip and strip (same functionality as python strings, big surprise)

--- include/ystring.h
+++ include/ystring.h
@@ -1,3 +1,33 @@
+/*
+Copyright (c) 2012, jb0n
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+*/
+
+
 #ifndef YSTRING_H_INCLUDED
 #define YSTRING_H_INCLUDED
 
@@ -9,10 +39,14 @@ namespace b0n {
 class ystring: public std::string {
 public:
     //these are the only pieces of functionality ystring has over std::string
-    ystring replace(const char*, const char*, int times=-1); //works like python
     ystring &format(const char *, ...); //vsnprintf wrapper
-    std::vector<ystring> split(ystring, int max_fields=-1); //works like python
 
+    //these are all python work-alikes.
+    ystring replace(const char*, const char*, int times=-1);
+    std::vector<ystring> split(ystring, int max_fields=-1);
+    ystring rstrip();
+    ystring lstrip();
+    ystring strip();
 
     //std::string c'tors
     ystring() {}
